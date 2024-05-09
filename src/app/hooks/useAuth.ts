dc99ffe2810921cebfc6256d770679042b3ce211
PhucNgo <eddsophine@gmail.com>
2024-05-09 07:52:03 +0700

    Update useAuth.ts

--- src/app/hooks/useAuth.ts
+++ src/app/hooks/useAuth.ts
@@ -1,24 +1,24 @@
-import { onAuthStateChanged } from 'firebase/auth'
-import React, { useEffect, useState } from 'react'
-import { auth } from '../config/firebase'
+import { onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { auth } from '../config/firebase';
 
 const useAuth = () => {
-    const [user,setLocalUer] = useState<Any | null>(null)
+    const [user, setUser] = useState<any | null>(null);
     
     useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, (user : any) => {
-          if(user){
-            console.log('there is a user')
-            setLocalUer(user)
-          }else{
-            console.log('no user')
-          }
-      })  
+        const unsubscribe = onAuthStateChanged(auth, (user: any) => {
+            if (user) {
+                console.log('there is a user');
+                setUser(user);
+            } else {
+                console.log('no user');
+            }
+        });
 
-      return () => unsubscribe()
-    },[])
+        return () => unsubscribe();
+    }, []);
     
-    return user
-}
+    return user;
+};
 
-export default useAuth
+export default useAuth;
